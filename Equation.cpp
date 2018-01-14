065bb1d77399110ed694f6722d02463aa6efc1a6
Chris <christopher.roy@mail.mcgill.ca>
2018-01-13 20:01:58 -0500

    Pushing local repo to github

---
+++ Equation.cpp
@@ -0,0 +1,18 @@
+/*
+ * Equation.cpp
+ *
+ *  Created on: 2018-01-13
+ *      Author: roysirch
+ */
+
+#include "Equation.h"
+
+Equation::Equation() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Equation::~Equation() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Equation.h
@@ -0,0 +1,17 @@
+/*
+ * Equation.h
+ *
+ *  Created on: 2018-01-13
+ *      Author: roysirch
+ */
+
+#ifndef EQUATION_H_
+#define EQUATION_H_
+
+class Equation {
+public:
+	Equation();
+	virtual ~Equation();
+};
+
+#endif /* EQUATION_H_ */
